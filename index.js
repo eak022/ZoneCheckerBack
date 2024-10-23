21161ea7ed8474906c26678c3f2bfcc054aa51fb
eak022 <121035260+eak022@users.noreply.github.com>
2024-10-24 01:32:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://zone-checker-front.vercel.app",
 };
 
 app.use(cors(corsOption));
